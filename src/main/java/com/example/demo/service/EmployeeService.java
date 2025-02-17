adb30480a017900f76044ac706751e2183277e45
Dongwook Kim <dongwook227@gmail.com>
2025-02-17 07:00:14 -0500

    create employee service interface

---
+++ src/main/java/com/example/demo/service/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    List<Employee> findAll();
+    Employee findById(int theId);
+    void save(Employee theEmployee);
+    void deleteById(int theId);
+}
